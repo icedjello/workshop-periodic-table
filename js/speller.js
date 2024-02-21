ac638e118bafa834893efb09fa59153e4b7434e8
icedjello <revues.counter0x@icloud.com>
2024-02-21 10:07:37 +0000

    added comments explaining what is going on in speller

--- js/speller.js
+++ js/speller.js
@@ -14,15 +14,23 @@ async function loadPeriodicTable() {
 }
 
 function check(inputWord) {
+    // check if there are characters.
+    // else return []
     if (inputWord.length > 0) {
+        // go through all elements in JSON object
         for (let element of elements) {
             let symbol = element.symbol.toLowerCase();
+            // if the symbol length is longer than inputWord -> skip
             if (symbol.length <= inputWord.length) {
+                // if the symbol matches the first 1 or 2 letters
                 if (inputWord.slice(0, symbol.length) == symbol) {
+                    // if there are still letters left in inputWord
                     if (inputWord.length > symbol.length) {
-                        let res = check(inputWord.slice(symbol.length));
-                        if (res.length > 0) {
-                            return [symbol, ...res];
+                        // recurse for rest of word
+                        let restOfWord = check(inputWord.slice(symbol.length));
+                        // if more symbols are found
+                        if (restOfWord.length > 0) {
+                            return [symbol, ...restOfWord];
                         }
                     } else {
                         return [symbol];
