c148bf73a4ed1d3b2bbf50329e5c42f46f4a3f0a
icedjello <revues.counter0x@icloud.com>
2024-02-21 13:24:20 +0000

    added some imporvements to speller

--- js/speller.js
+++ js/speller.js
@@ -4,6 +4,8 @@ export default {
 };
 
 var elements;
+var symbols = {};
+// { au : { "name": "Gold",...} }
 
 await loadPeriodicTable();
 
@@ -11,41 +13,109 @@ await loadPeriodicTable();
 
 async function loadPeriodicTable() {
     elements = await (await fetch("periodic-table.json")).json();
+    for (let element of elements) {
+        symbols[element.symbol.toLowerCase()] = element;
+    }
 }
 
-function check(inputWord) {
-    // check if there are characters.
-    // else return []
-    if (inputWord.length > 0) {
-        // go through all elements in JSON object
-        for (let element of elements) {
-            let symbol = element.symbol.toLowerCase();
-            // if the symbol length is longer than inputWord -> skip
-            if (symbol.length <= inputWord.length) {
-                // if the symbol matches the first 1 or 2 letters
-                if (inputWord.slice(0, symbol.length) == symbol) {
-                    // if there are still letters left in inputWord
-                    if (inputWord.length > symbol.length) {
-                        // recurse for rest of word
-                        let restOfWord = check(inputWord.slice(symbol.length));
-                        // if more symbols are found
-                        if (restOfWord.length > 0) {
-                            return [symbol, ...restOfWord];
-                        }
-                    } else {
-                        return [symbol];
+function findCandidates3(inputWord) {
+    var oneLetterSymbols = [];
+    var twoLetterSymbols = [];
+
+    for (let i = 0; i < inputWord.length; i++) {
+        // collect one letter symbol options
+        if (
+            inputWord[i] in symbols &&
+            !oneLetterSymbols.includes(inputWord[i])
+        ) {
+            oneLetterSymbols.push(inputWord[i]);
+        }
+
+        // collect two letter symbol options
+        if (i <= inputWord.length - 2) {
+            let two = inputWord.slice(i, i + 2);
+            if (two in symbols && !twoLetterSymbols.includes(two)) {
+                twoLetterSymbols.push(two);
+            }
+        }
+    }
+
+    return [...twoLetterSymbols, ...oneLetterSymbols];
+}
+
+function findCandidates(inputWord) {
+    var oneLetterSymbols = [];
+    var twoLetterSymbols = [];
+    for (let i = 0; i < inputWord.length; i++) {
+        // check if the single letter exist in symbols
+        // and check for doubles: *Y* U C K *Y*
+        // do not double add *Y*
+        if (
+            inputWord[i] in symbols &&
+            !oneLetterSymbols.includes(inputWord[i])
+        ) {
+            oneLetterSymbols.push(inputWord[i]);
+        }
+        // stop one letter before end because
+        // we are matching two at a time
+        if (i <= inputWord.length - 2) {
+            let two = inputWord.slice(i, i + 2);
+            // same as above
+            if (two in symbols && !twoLetterSymbols.includes(two)) {
+                twoLetterSymbols.push(two);
+            }
+        }
+    }
+    // The order of the return matters because it determines
+    // if one or two letter symbols are preferred
+    // two letters preferred here
+    return [...twoLetterSymbols, ...oneLetterSymbols];
+}
+
+function spellWord(candidates, charactersLeft) {
+    console.log({ candidates });
+    if (charactersLeft.length == 0) {
+        return [];
+    } else {
+        if (charactersLeft.length >= 2) {
+            let two = charactersLeft.slice(0, 2);
+            let rest = charactersLeft.slice(2);
+            // if match found
+            if (candidates.includes(two)) {
+                // more characters to match
+                if (rest.length > 0) {
+                    let result = [two, ...spellWord(candidates, rest)];
+                    if (result.join("") == charactersLeft) {
+                        return result;
                     }
+                } else {
+                    return [two];
                 }
+            }
+        }
+    }
+    if (charactersLeft.length >= 1) {
+        let one = charactersLeft[0];
+        let rest = charactersLeft.slice(1);
+        if (candidates.includes(one)) {
+            if (rest.length > 0) {
+                let result = [one, ...spellWord(candidates, rest)];
+                if (result.join("") == charactersLeft) {
+                    return result;
+                }
+            } else {
+                return [one];
             }
         }
     }
     return [];
 }
 
+function check(inputWord) {
+    var candidates = findCandidates(inputWord);
+    return spellWord(candidates, inputWord);
+}
+
 function lookup(elementSymbol) {
-    for (let element of elements) {
-        if (element.symbol.toLowerCase() == elementSymbol) {
-            return element;
-        }
-    }
+    return symbols[elementSymbol];
 }
